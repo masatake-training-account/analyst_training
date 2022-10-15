03071cd370794f19d49b9f0c0af4d82113e521e1
masatake74 <masatake.home@outlook.jp>
2022-10-15 15:23:11 +0900

    knock04完了

--- knock/knock04.py
+++ knock/knock04.py
@@ -1,6 +1,6 @@
 """
-transactionデータを列方向に結合して列の長さを表示する
 列データが重複しないようにmergeを使用して結合
+customer_masterとitem_masterをjoin_dataに結合する。
 
 データ：
 * customer_master.csv
@@ -14,8 +14,13 @@ transactionデータを列方向に結合して列の長さを表示する
 import pandas as pd
 
 
-def knock03():
+def knock04():
 	# データを読み込み
+	# 顧客データを表示する
+	customer_master = pd.read_csv(r'./knock/knock01/customer_master.csv')
+	# 取り扱っている商品データ
+	item_master = pd.read_csv(r'./knock/knock01/item_master.csv')
+
 	# 購入明細データ1
 	transaction_1 = pd.read_csv(r'./knock/knock01/transaction_1.csv')
 	# 購入明細データ2
@@ -29,10 +34,6 @@ def knock03():
 	transaction = pd.concat((transaction_1, transaction_2), axis=0, ignore_index=True)
 	transaction_detail = pd.concat((transaction_detail_1, transaction_detail_2), axis=0, ignore_index=True)
 
-	# 列名の確認
-	print(transaction.columns)
-	print(transaction_detail.columns)
-
 	# 列方向に結合
 	# transaction_idが重複
 	join_data = pd.merge(
@@ -40,9 +41,26 @@ def knock03():
 			['transaction_id', 'payment_date', 'customer_id']
 		], on="transaction_id", how="left")
 
-	# 結果の表示
-	print(join_data)
+	# join_dataの列を確認
+	print(join_data.columns)
 
-	print(len(transaction_detail))
-	print(len(transaction))
-	print(len(join_data))
+	# 列データを確認する
+	print(customer_master.columns)
+	print(item_master.columns)
+
+	# 消費者データの結合
+	join_data = pd.merge(
+		join_data, customer_master, on="customer_id", how="left"
+	)
+
+	# 商品データの結合
+	join_data = pd.merge(
+		join_data, item_master, on="item_id", how="left"
+	)
+
+	# 結果の出力
+	print(join_data.head())
+	print(join_data.columns)
+
+	# # csvファイルで保存
+	# join_data.to_csv('knock04.csv', encoding="shift_jis")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import knock.knock03
+import knock.knock04
 
 # ガター内の緑色のボタンを押すとスクリプトを実行します。
 if __name__ == '__main__':
 	print('python実践データ分析')
 
-	knock.knock03.knock03()
+	knock.knock04.knock04()
